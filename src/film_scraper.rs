6a78a9af593ade193a71f22ede3b4728bccdcf66
Tourlat <foucaultfelix33@gmail.com>
2024-07-26 16:44:18 +0200

    Added tests in film_scrappers.rs

--- src/film_scraper.rs
+++ src/film_scraper.rs
@@ -105,3 +105,129 @@ pub fn extract_film_meta_datas(film_name: &str) -> Result<FilmMetaData, FilmErro
         genres,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_build_film_url() {
+        let film_name = "Mad max: fury road";
+        let expected_url = "https://letterboxd.com/film/mad-max:-fury-road";
+        assert_eq!(build_film_url(film_name), expected_url);
+    }
+
+    #[test]
+    fn test_get_film_rating() {
+        let html_content = r#"
+            <html>
+                <head>
+                    <meta name="twitter:data2" content="8.5 / 10">
+                </head>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        let selector = Selector::parse(r#"meta[name="twitter:data2"]"#).unwrap();
+        assert_eq!(get_film_rating(&document, &selector).unwrap(), 8.5);
+    }
+
+    #[test]
+    fn test_get_film_single_info() {
+        let html_content = r#"
+            <html>
+                <body>
+                    <div class="releaseyear">
+                        <a>2008</a>
+                    </div>
+                </body>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        let selector = Selector::parse("div.releaseyear a").unwrap();
+        assert_eq!(get_film_single_info(&document, &selector).unwrap(), "2008");
+    }
+
+    #[test]
+    fn test_get_film_multiples_infos() {
+        let html_content = r#"
+            <html>
+                <body>
+                    <div class="text-sluglist capitalize">
+                        <a class="text-slug">Action</a>
+                        <a class="text-slug">Crime</a>
+                        <a class="text-slug">Drama</a>
+                    </div>
+                </body>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        let selector = Selector::parse("div.text-sluglist.capitalize a.text-slug").unwrap();
+        assert_eq!(
+            get_film_multiples_infos(&document, &selector).unwrap(),
+            vec!["Action", "Crime", "Drama"]
+        );
+    }
+
+    #[test]
+    fn test_extract_release_year() {
+        let html_content = r#"
+            <html>
+                <body>
+                    <div class="releaseyear">
+                        <a>1984</a>
+                    </div>
+                </body>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        assert_eq!(extract_release_year(&document).unwrap(), "1984");
+    }
+
+    #[test]
+    fn test_extract_director() {
+        let html_content = r#"
+            <html>
+                <body>
+                    <span class="directorlist">
+                        <a>
+                            <span class="prettify">Akira Kurosawa</span>
+                        </a>
+                    </span>
+                </body>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        assert_eq!(extract_director(&document).unwrap(), "Akira Kurosawa");
+    }
+
+    #[test]
+    fn test_extract_synopsis() {
+        let html_content = r#"
+            <html>
+                <body>
+                    <div class="truncate" data-truncate="450">
+						<p>A cool synopsis</p>
+					</div>
+                </body>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        assert_eq!(
+            extract_synopsis(&document).unwrap(),
+            "A cool synopsis"
+        );
+    }
+
+    #[test]
+    fn test_extract_rating() {
+        let html_content = r#"
+            <html>
+                <head>
+                    <meta name="twitter:data2" content="4.5 / 5">
+                </head>
+            </html>
+        "#;
+        let document = Html::parse_document(html_content);
+        assert_eq!(extract_rating(&document).unwrap(), 4.5);
+    }
+}
